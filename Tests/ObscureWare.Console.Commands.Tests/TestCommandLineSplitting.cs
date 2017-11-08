26a32672c60e5ce5a4fbcd3a0ee47fab797d053e
Sebastian-Gruchacz <sebgruch@gmail.com>
2017-11-08 12:28:00 +0100

    Added command line test projects with some basic tests for splitting class.

---
+++ Tests/ObscureWare.Console.Commands.Tests/TestCommandLineSplitting.cs
@@ -0,0 +1,38 @@
+﻿namespace ObscureWare.Console.Commands.Tests
+{
+    using System.Linq;
+
+    using ObscureWare.Console.Shared;
+
+    using Shouldly;
+
+    using Xunit;
+
+    public class TestCommandLineSplitting
+    {
+
+        [Theory]
+        [InlineData(@"", new string[] { })]
+        [InlineData(@"a", new string[] { "a" })]
+        [InlineData(@"'a'", new string[] { "'a'" })]
+        [InlineData("\"a\"", new string[] { "a" })]
+        public void single_command_shall_be_parsed_properly_in_different_scenarios(string cmd, string[] expected)
+        {
+            var results = CommandLineUtilities.SplitCommandLine(cmd).ToArray();
+
+            results.ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData(@"--a a", new string[] { "--a", "a" })]
+        [InlineData(@"--a 20", new string[] { "--a", "20" })]
+        [InlineData(@"-abc 'a'", new string[] { "-abc", "'a'" })]
+        [InlineData("--bcd \"a\"", new string[] { "--bcd", "a" })]
+        public void tuple_command_shall_be_parsed_properly_in_different_scenarios(string cmd, string[] expected)
+        {
+            var results = CommandLineUtilities.SplitCommandLine(cmd).ToArray();
+
+            results.ShouldBe(expected);
+        }
+    }
+}
