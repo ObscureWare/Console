dc2eb1379d6dc2a589b80e6566d1935403a41344
Sebastian-Gruchacz <sebgruch@gmail.com>
2017-11-10 10:56:35 +0100

    Float / double localized parsing command tests.

--- Tests/ObscureWare.Console.Commands.Tests/LocalizedParsingTests.cs
+++ Tests/ObscureWare.Console.Commands.Tests/LocalizedParsingTests.cs
@@ -1,25 +1,86 @@
 ﻿namespace ObscureWare.Console.Commands.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    using ObscureWare.Console.Commands.Engine;
+    using ObscureWare.Console.Commands.Engine.Internals;
+    using ObscureWare.Console.Commands.Engine.Styles;
+    using ObscureWare.Console.Commands.Interfaces;
+    using ObscureWare.Console.Commands.Interfaces.Model;
+    using ObscureWare.Console.Commands.Tests.TestModels;
+    using ObscureWare.Console.Root.Interfaces;
+    using ObscureWare.Console.Shared;
+
+    using Shouldly;
+
     using Xunit;
 
     public class LocalizedParsingTests
     {
+        private static IConsole console = new TestConsole();
+
         [Theory]
         [InlineData("en-US", "ParseFloats -a 12.445 -b 23.454")]
         [InlineData("pl-PL", "ParseFloats -a 12,445 -b 23,454")]
         public void float_and_decimal_values_shall_be_parsed_correctly_according_to_culture(string cultureName, string cmd)
         {
-            
-        }
+            var parserOptions = BuildParserOptions(cultureName);
+            var modelType = typeof(FloatPropertiesTestCommandModel);
+            var commandType = typeof(FloatPropertiesTestCommand);
 
+            var expectedModel = new FloatPropertiesTestCommandModel
+            {
+                A = 12.445,
+                B = 23.454f
+            };
+
+            var validatingCommandLine = CommandLineUtilities.SplitCommandLine(cmd).ToArray(); ;
+
+            var cmdManager = new CommandManager(new Type[] { commandType }, new DefaultCommandResolver());
+            var outputManager = new OutputManager(console, CommandEngineStyles.DefaultStyles, CultureInfo.InvariantCulture);
+
+            var cmdInfo = cmdManager.FindCommand(validatingCommandLine[0]);
+            var model = this.BuildModelForCommand(cmdInfo, validatingCommandLine.Skip(1), parserOptions, outputManager);
+
+            model.ShouldNotBeNull();
+            model.ShouldBeOfType(expectedModel.GetType());
+            model.ShouldBe(expectedModel);
+        }
 
         [Theory]
-        [InlineData("en-US", "ParseFloats -d 12/14/2017 -t 10:23 -dt \"12/14/2017 10:23:14\"")]
-        [InlineData("pl-PL", "ParseFloats -d 14/12/2017 -t 10:23 -dt \"14/12/2017 10:23:14\"")]
+        [InlineData("en-US", "ParseDateTime -d 12/14/2017 -t 10:23 -dt \"12/14/2017 10:23:14\"")]
+        [InlineData("pl-PL", "ParseDateTime -d 14/12/2017 -t 10:23 -dt \"14/12/2017 10:23:14\"")]
         public void date_and_time_values_shall_be_parsed_correctly_according_to_culture(string cultureName, string cmd)
         {
+            var simpleSeparatedParsingOptions = BuildParserOptions(cultureName);
 
         }
 
+
+
+        private object BuildModelForCommand(CommandInfo cmdInfo, IEnumerable<string> arguments, ICommandParserOptions options, ICommandOutput outputManager)
+        {
+            return cmdInfo.CommandModelBuilder.BuildModel(arguments, options, outputManager);
+        }
+
+        private static ICommandParserOptions BuildParserOptions(string cultureName)
+        {
+            ICommandParserOptions simpleSeparatedParsingOptions = new CommandParserOptions
+            {
+                UiCulture = CultureInfo.CreateSpecificCulture(cultureName),
+                FlagCharacters = new[] { @"\" },
+                SwitchCharacters = new[] { @"-" },
+                OptionArgumentMode = CommandOptionArgumentMode.Separated,
+                OptionArgumentJoinCharacater = ':',
+                AllowFlagsAsOneArgument = false,
+                CommandsSensitivenes = CommandCaseSensitivenes.Insensitive,
+                SwitchlessOptionsMode =
+                    SwitchlessOptionsMode.EndOnly
+            };
+            return simpleSeparatedParsingOptions;
+        }
     }
 }
---
+++ Tests/ObscureWare.Console.Commands.Tests/TestModels/FloatPropertiesTestCommand.cs
@@ -0,0 +1,57 @@
+﻿namespace ObscureWare.Console.Commands.Tests.TestModels
+{
+    using System;
+
+    using ObscureWare.Console.Commands.Interfaces;
+    using ObscureWare.Console.Commands.Interfaces.Model;
+
+    [CommandModel(typeof(FloatPropertiesTestCommandModel))]
+    public class FloatPropertiesTestCommand : IConsoleCommand
+    {
+        public void Execute(object contextObject, ICommandOutput output, object runtimeModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [CommandName("ParseFloats")]
+    [CommandDescription(@"float parsing test model")]
+    [CommandModelFor(typeof(FloatPropertiesTestCommand))]
+    public class FloatPropertiesTestCommandModel : CommandModel, IEquatable<FloatPropertiesTestCommandModel>
+    {
+        [CommandOptionName(@"a")]
+        [Mandatory(false)]
+        [CommandOptionCustomValueSwitch("a")]
+        [CommandDescription("a")]
+        public double A { get; set; }
+
+        [CommandOptionName(@"b")]
+        [Mandatory(false)]
+        [CommandOptionCustomValueSwitch("b")]
+        [CommandDescription("a")]
+        public float B { get; set; }
+
+        public bool Equals(FloatPropertiesTestCommandModel other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return A.Equals(other.A) && B.Equals(other.B);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((FloatPropertiesTestCommandModel)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (A.GetHashCode() * 397) ^ B.GetHashCode();
+            }
+        }
+    }
+}
