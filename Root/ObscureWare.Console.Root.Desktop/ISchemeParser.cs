65aa318134c2561f01055757e393ba8322d663ba
Sebastian Gruchacz <sebastian@gruchacz.com>
2018-05-30 00:36:17 +0200

    Raw import of ColorTool code from https://github.com/Microsoft/console/tree/master/tools/ColorTool. Compilation broken due to not importing resources.

---
+++ Root/ObscureWare.Console.Root.Desktop/ISchemeParser.cs
@@ -0,0 +1,16 @@
+﻿namespace ObscureWare.Console.Root.Desktop
+{
+    // Copyright (C) Microsoft.  All rights reserved.
+    // Licensed under the terms described in the LICENSE file in the root of this project.
+    //
+
+    // Based on e-MIT-ed source from https://github.com/Microsoft/console/blob/master/tools/ColorTool/ColorTool/ISchemeParser.cs
+    // Adapted to be used and reuse ObscureWare's Console library
+
+    interface ISchemeParser
+    {
+        string Name { get; }
+
+        ColorScheme ParseScheme(string schemeName, bool reportErrors = true);
+    }
+}
