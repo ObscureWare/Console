6cb4faf9b9c1eaf43c9ae7c8a8fd9f7d978219f8
Sebastian Gruchacz <sebastian@gruchacz.com>
2018-06-04 00:07:45 +0200

    Added some code to console menu.

--- Operations/ObscureWare.Console.Operations.Gaming/Menu/ConsoleMenu.cs
+++ Operations/ObscureWare.Console.Operations.Gaming/Menu/ConsoleMenu.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Drawing;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Root.Shared;
@@ -12,41 +13,97 @@
     /// </summary>
     public class ConsoleMenu
     {
-        private readonly IConsole _console;
+        private readonly IAtomicConsole _console;
         private readonly Rectangle _availableArea;
-        private readonly IEnumerable<ConsoleMenuItem> _menuItems;
+        private readonly ConsoleMenuItem[] _menuItems;
         private readonly MenuStyles _styling;
+        private ConsoleMenuItem _selectedItem = null;
+        private ConsoleMenuItem _activeItem = null;
 
-        public ConsoleMenu(IConsole console, Rectangle availableArea, IEnumerable<ConsoleMenuItem> menuItems, MenuStyles styling)
+        public ConsoleMenu(IAtomicConsole console, Rectangle availableArea, IEnumerable<ConsoleMenuItem> menuItems, MenuStyles styling)
         {
-            this._console = console;
+            this._console = console ?? throw new ArgumentNullException(nameof(console));
             this._availableArea = availableArea;
-            this._menuItems = menuItems;
-            this._styling = styling;
+            this._styling = styling ?? throw new ArgumentNullException(nameof(styling));
+
+            if (menuItems != null)
+            {
+                this._menuItems = menuItems.ToArray();
+                this._selectedItem = this._menuItems.FirstOrDefault();
+            }
+            else
+            {
+                this._menuItems = new ConsoleMenuItem[0];
+            }
         }
 
         public void Render()
         {
-            throw new NotImplementedException();
+            int yindex = 0;
+            foreach (var item in this._menuItems)
+            {
+                // TODO: use Span<char> to optimize. Hopefully System.Console will receive such overload as well
+                var caption = (item.Caption.Length <= this._availableArea.Width)
+                    ? item.Caption.PadRight(this._availableArea.Width, ' ') // want bg-color replacement after text as well
+                    : item.Caption.Substring(0, this._availableArea.Width); // TODO: add elipsis? set style?
+
+                this._console.RunAtomicOperations((c) =>
+                {
+                    c.SetCursorPosition(this._availableArea.X, this._availableArea.Y + yindex++);
+                    c.WriteText(this.ChooseItemStyle(item), caption);
+                });
+            }
         }
 
         /// <summary>
-        /// Activates menu
+        /// Activates menu.
         /// </summary>
-        /// <returns></returns>
-        public async Task<ConsoleMenuItem> Activate()
+        /// <returns>Finally activated menu item.</returns>
+        public ConsoleMenuItem Focus() // TODO: async?
         {
-            throw new NotImplementedException();
+            ConsoleKey key = ConsoleKey.Escape;
+            while (key != this._styling.SelectionKey)
+            {
+                key = this._console.ReadKey().Key;
+
+                // TODO: add selection moving + rendering (changed lines only) routines
+            }
+
+            this._activeItem = this._selectedItem;
+            return this._activeItem;
         }
 
         /// <summary>
-        /// Raised when user moves through menu
+        /// Raised when user moves through menu, yet not activating items
         /// </summary>
         public event MenuItemChangedHandler ItemChanged;
 
         protected virtual void OnItemChanged(MenuItemChangedHandlerArgs args)
         {
             this.ItemChanged?.Invoke(this, args);
+        }
+
+        private ConsoleFontColor ChooseItemStyle(ConsoleMenuItem item)
+        {
+            if (item.Enabled)
+            {
+                if (this._selectedItem == item)
+                {
+                    return this._styling.SelectedItem;
+                }
+                else if (this._activeItem == item)
+                {
+                    return this._styling.ActiveItem;
+                }
+                else
+                {
+                    return this._styling.NormalItem;
+                }
+            }
+            else
+            {
+                return this._styling.DisabledItem;
+            }
         }
     }
 }
